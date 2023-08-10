7e37c45bc2bf624f02ca046fca1ac7976aae4d8c
Nyxfor13days <nyxfor13days@gmail.com>
2023-08-10 19:51:18 +0530

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+export const GITHUB_DEV_URL =
+  "https://github.com/nyxfor13days/tools.nyxfor13days.com";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    link: "https://github.com/nyxfor13days",
+  },
+];
